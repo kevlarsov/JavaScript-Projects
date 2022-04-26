20b73de554e6918c536281d85f78b526db865ad6
kevlarsov <103957894+kevlarsov@users.noreply.github.com>
2022-04-26 13:14:32 -0600

    more basic JS tests

--- Basic_JS_Projects/project1_expressions_alert/js/main.js
+++ Basic_JS_Projects/project1_expressions_alert/js/main.js
@@ -1,14 +1,28 @@
-var x = "X gon' give it to ya!"
+//assigned string to var 'x'
+let x = "X gon' give it to ya!";
 
+//alert pop-up with var 'x'
 window.alert(x);
 
-var y = x + " He gon' give it to ya! "
+//concatenated additional string to var 'x' to make var 'y'
+let y = x + " He gon' give it to ya! ";
 
-document.write(y)
+//first body text
+document.write(y);
 
+//second body text
 document.write("\"And I will strike down upon thee with great vengeance and FURIOUS anger, "
     +"those who attempt to poison and destroy my brothers!\" exclaimed Jules. ");
 
-var family = "The Jetsons", dad = "George", mom = "Jane", daughter = "Judy", son = "Elroy", dog = "Astro", maid = "Rosie";
+//multiple variable, one statement
+let family = "The Jetsons ", dad = "George ", mom = "Jane ", daughter = "Judy ", son = "Elroy ", dog = "Astro ", maid = "Rosie ";
 
+//third, selected text
 document.write(dog);
+
+//random variables
+let z = 1794;
+let a = 29065;
+
+//expression
+document.write(a+z);
