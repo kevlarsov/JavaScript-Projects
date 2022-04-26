03fb0f9b28b019dc795a5fad6a82351574f87322
kevlarsov <103957894+kevlarsov@users.noreply.github.com>
2022-04-26 11:28:35 -0600

    testing basic JS

---
+++ Basic_JS_Projects/project1_expressions_alert/js/main.js
@@ -0,0 +1,14 @@
+var x = "X gon' give it to ya!"
+
+window.alert(x);
+
+var y = x + " He gon' give it to ya! "
+
+document.write(y)
+
+document.write("\"And I will strike down upon thee with great vengeance and FURIOUS anger, "
+    +"those who attempt to poison and destroy my brothers!\" exclaimed Jules. ");
+
+var family = "The Jetsons", dad = "George", mom = "Jane", daughter = "Judy", son = "Elroy", dog = "Astro", maid = "Rosie";
+
+document.write(dog);
