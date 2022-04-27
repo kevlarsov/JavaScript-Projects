fd0eaf65d2e28884c63830b21407a8fe6a0c1df5
kevlarsov <103957894+kevlarsov@users.noreply.github.com>
2022-04-27 14:33:14 -0600

    testing more basic JS

---
+++ Basic_JS_Projects/project2_functions/js/main.js
@@ -0,0 +1,12 @@
+//BUTTON FUNC
+function getShorty() {
+    let result = "GOT HIM!";
+    document.getElementById("shorty").innerHTML = result;
+}
+
+//TEXT CHANGE FUNC
+function dafunc() {
+    var sentence = "BAM!";
+    sentence += " \"Aw man I shot Marvin in the face...\"";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
