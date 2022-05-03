4e8580fa13b28152bade9369d5d1b4934b7f5a3e
kevlarsov <103957894+kevlarsov@users.noreply.github.com>
2022-05-02 19:47:52 -0600

    JS conditional/branching practice

--- Basic_JS_Projects/project7_scope/main.js
+++ Basic_JS_Projects/project7_scope/main.js
@@ -1,5 +1,7 @@
-//GLOBAL VAR
-let x = "Loading test results... ";
+//GLOBAL VARS
+let x, y;
+x = "Loading test results... ";
+y = -12;
 
 //TESTS FOR INPUT = "yes"
 function testfunc() {
@@ -8,4 +10,37 @@ function testfunc() {
     input = document.getElementById("input").value;
     result = (input == "yes") ? "PASS!":"FAIL!";
     document.getElementById("out").innerHTML = x + result;
+}
+
+//TIME-DEPENDENT OUTPUT
+function getdate() {
+    if (new Date().getHours() < 23) {
+        document.getElementById("greeting").innerHTML = "How are you today?";
+    }
+}
+
+//CUSTOM BRANCHED OUTPUT
+function rando() {
+    if (y > 0) {
+        document.getElementById("wubalubadubdub").innerHTML = "You're this week's winner!";
+    }
+    else {
+        document.getElementById("wubalubadubdub").innerHTML = "Better luck next time, pal!"
+    }
+}
+
+//TIME OF DAY OUTPUT
+function timefunc() {
+    let time = new Date().getHours();
+    let reply;
+        if (time < 12 == time > 0) {
+            reply = "Morning";
+        }
+        else if (time >= 12 == time < 17) {
+            reply = "Afternoon";
+        }
+        else {
+            reply = "Evening";
+        }
+    document.getElementById("tod").innerHTML = reply;
 }
