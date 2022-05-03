cbc05a4fea6795bc148adafaeef7ca03e144b89c
kevlarsov <103957894+kevlarsov@users.noreply.github.com>
2022-05-03 13:13:57 -0600

    while/for loops

--- Basic_JS_Projects/project10_loops_arrays/main.js
+++ Basic_JS_Projects/project10_loops_arrays/main.js
@@ -1,3 +1,22 @@
+//STRING LENGTH
+function sl() {
+    let text = "Whale hello there";
+    let length = text.length;
+    document.getElementById("out").innerHTML = length;
+}
+
+//CUSTOM FOR LOOP
+let cars = ["Audi", "BMW", "Jaguar", "Mercedes-Benz", "Porsche", "Volkswagen"];
+let content = "";
+let y;
+function for_loop() {
+    for (y = 0; y < cars.length; y++) {
+        content += cars[y] + "<br>";
+    }
+    document.getElementById("carlist").innerHTML = content;
+}
+
+//1-100 WHILE LOOP
 function call_loop() {
     let counter = "";
     let x = 1;
